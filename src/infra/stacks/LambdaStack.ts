191ae1fbe1c4c3591d1356b978b3f7f65bc0c1b4
adwxithc <adwaithjanardhanan0@gmail.com>
2026-04-16 11:31:26 +0530

    feat: update dependencies and refactor Lambda function
    - Added new dependencies for AWS SDK and TypeScript types in package.json.
    - Updated API endpoint in spaces.http.
    - Refactored Lambda function to use NodejsFunction from aws-lambda-nodejs and changed the handler implementation to TypeScript.
    - Removed the old hello.js file and created a new hello.ts file with S3 bucket listing functionality.
    - Added IAM policy to allow S3 bucket listing for the Lambda function.

--- src/infra/stacks/LambdaStack.ts
+++ src/infra/stacks/LambdaStack.ts
@@ -1,7 +1,9 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
-import { Code, Function as LambdaFuncation, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { Runtime } from "aws-cdk-lib/aws-lambda";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "node:path";
 
@@ -19,14 +21,23 @@ export class LambdaStack extends Stack {
     constructor(scope: Construct, id: string, props?: LambdaStackProps) {
         super(scope, id, props);
 
-        const helloLambda = new LambdaFuncation(this, 'HelloFunction', {
+        const helloLambda = new NodejsFunction(this, 'HelloFunction', {
             runtime: Runtime.NODEJS_22_X,
-            handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+            handler: 'handler',
+            entry: (join(__dirname, '..', '..', 'services', 'hello.ts')),
             environment: {
                 TABLE_NAME: props?.spacesTable.tableName || ''
             }
         });
+
+        helloLambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                's3:ListAllMyBuckets',
+                's3:ListBucket'
+            ],
+            resources: ['*'] //bad practice, but for demo purposes it's fine
+        }))
 
         this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
 
--- src/services/hello.js
+++
@@ -1,8 +0,0 @@
-exports.main = async function (event, context) {
-    return{
-        statusCode: 200,
-        body: JSON.stringify({
-            message:`Hello i will read from the database table ${process.env.TABLE_NAME}`
-        })
-    }
-};
